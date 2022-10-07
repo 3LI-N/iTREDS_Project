256d1a8ea8f265a81afe29ea696079dc8f41596d
3LI-N <enord@DESKTOP-5KKVVIE.localdomain>
2022-10-06 20:28:59 -0400

    checks for project design reports and automatically generates txt

--- ifad/ifad_scraper.py
+++ ifad/ifad_scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from urllib.request import unquote
+import os
 
 class Project:
 	def __init__(self, title, country, year, id, url):
@@ -10,21 +11,27 @@ class Project:
 		self.year = year
 		self.id = id
 		self.url = url
-		self.pres_link = ''
+		self.doc_link = ''
+		self.has_pres_report = False
+
 	def print_data(self):
 		print("Title: " + self.title)
 		print("Country: " + self.country)
 		print("Year: " + self.year)
 		print("ID: " + self.id)
 		print("Url: " + self.url)
-		if self.pres_link == '':
-			print("There is no President's report link")
+		if self.doc_link == '':
+			print("There is no report link")
+		elif self.has_pres_report:
+			print("President's Report link: " + self.doc_link)
 		else:
-			print("Presidents report link: " + self.pres_link)
+			print("Project Design Report link: " + self.doc_link)
+
 	def get_filename(self):
-		filename = self.country + "_" + self.year + "_" + self.id + ".pdf"
+		filename = self.country + "_" + self.year + "_" + self.id
 		return filename.replace(" ", "-").lower()
-	def get_presidents_report_link(self):
+
+	def get_report_link(self):
 		response = requests.get(self.url)
 		content = BeautifulSoup(response.text, 'lxml')
 
@@ -44,58 +51,53 @@ class Project:
 				for text in link_texts:
 					cmp_text = str(text).replace("’","'").lower()
 					if "president's report" in cmp_text:
-						#print('found a match')
-						#print(http_match[0])
-						self.pres_link = http_match[0]
+						self.doc_link = http_match[0]
+						self.has_pres_report = True
 						return
+					elif "design" in cmp_text:
+						if self.doc_link == '':
+							self.doc_link = http_match[0]
 			except:
 				pass
 
+	def download_txt(self):
+		if self.doc_link == '':
+			print("Couldn't find a report")
+			return
+		if self.has_pres_report:
+			print("Downloading President's Report")
+		else:
+			print("Downloading Project Design Report")
 
-def download_pdf(project):
-	# run pdf2txt.py ./pdf/<pdf name> > ./txt_files/<txt name>
-	#url = 'https://www.ifad.org/en/-/haiti-2000002247-i-be-project-design-report-november-2021'
+		response = requests.get(self.doc_link)
+		content = BeautifulSoup(response.text, 'lxml')
 
-	if project.pres_link == '':
-		return
-	response = requests.get(project.pres_link)
-	content = BeautifulSoup(response.text, 'lxml')
+		pdf_urls = content.find_all('button')
 
-	pdf_urls = content.find_all('button')
-	#print(pdf_urls)
+		pdf_link = ''
+		for url in pdf_urls:
+			try:
+				full_http_match = re.findall('https.+\.pdf', url['onclick'])
+				if len(full_http_match) > 0:
+					pdf_link = full_http_match[0]
+				else:
+					partial_http_match = re.search(r"window.open\(\'(.+)\',", url['onclick'])
+					if partial_http_match != None:
+						pdf_link = 'https://ifad.org' + partial_http_match.group(1)
+			except:
+				pass
 
-	#print('\nJust the link:')
-
-	t = 'http'
-	pdf_link = ''
-	for url in pdf_urls:
-		try:
-			full_http_match = re.findall('https.+\.pdf', url['onclick'])
-			if len(full_http_match) > 0:
-				pdf_link = full_http_match[0]
-				#print(pdf_link)
-			else:
-				#print('in this else statement')
-				partial_http_match = re.search(r"window.open\(\'(.+)\',", url['onclick'])
-				#print('got past here')
-				if partial_http_match != None:
-					#print('there is a partial http match')
-					#print(partial_http_match.group(1))
-					pdf_link = 'https://ifad.org' + partial_http_match.group(1)
-		except:
-			pass
-
-	if len(pdf_link) > 0:
-		pdf_response = requests.get(pdf_link)
-		filename = project.get_filename()
-		print("PDF generated: " + filename)
-		with open('./pdf/' + filename, 'wb') as f:
-			f.write(pdf_response.content)
-
-
-
-
-
+		if len(pdf_link) > 0:
+			pdf_response = requests.get(pdf_link)
+			filename = str(self.get_filename())
+			pdf_filename = filename + ".pdf"
+			txt_filename = filename + ".txt"
+			with open('./pdf/' + pdf_filename, 'wb') as f:
+				f.write(pdf_response.content)
+			print("PDF generated: " + pdf_filename)
+			os.system("pdf2txt.py ./pdf/" + pdf_filename + " > ./txt_files/" + txt_filename)
+			print("txt generated: " + txt_filename)
+			os.system("rm ./pdf/" + pdf_filename) # saves space by deleting the pdf
 
 
 def get_valid_project_urls(oldest_year):
@@ -133,29 +135,45 @@ current_projects = get_valid_project_urls(2015)
 print("Number of projects: " + str(len(current_projects)))
 print()
 
-#pres_report_projects = []
-#no_pres_report_projects = []
+'''report_projects = []
+no_report_projects = []
 
-'''for i, project in enumerate(current_projects):
-	project.get_presidents_report_link()
-	if project.pres_link == '':
-		no_pres_report_projects.append(project)
-		print(str(i) + "\tno")
+num_pres = 0
+num_dsgn = 0
+
+for i, project in enumerate(current_projects):
+	project.get_report_link()
+	if project.doc_link == '':
+		no_report_projects.append(project)
+		print(str(i) + "\tno report")
 	else:
-		pres_report_projects.append(project)
-		print(str(i) + "\tyes")
+		report_projects.append(project)
+		if project.has_pres_report:
+			print(str(i) + "\tpresident's report")
+			num_pres += 1
+		else:
+			print(str(i) + "\tproject design report")
+			num_dsgn += 1
 print()
-print(str(len(pres_report_projects)) + " projects have a president's report")			#102
-print(str(len(no_pres_report_projects)) + " projects don't have a president's report")	#101
+print(str(num_pres) + " projects have a president's report")								#102
+print(str(num_dsgn) + " projects have a project design report but no president's reprot")	#61
+print(str(len(no_report_projects)) + " projects don't have a president's report")			#40
 '''
 
-test_project = current_projects[40] # [3] is an example with both presidents report and project design report
+test_project = current_projects[199] # [3] is an example with both presidents report and project design report
 # using 35 causes an error
-test_project.get_presidents_report_link()
+test_project.get_report_link()
 print()
 test_project.print_data()
 print()
-if test_project.pres_link == '':
-	print("Couldn't find President's Report")
-else:
-	download_pdf(test_project)
+test_project.download_txt()
+
+
+'''
+Issues:
+- some president's reports don't have the phrase "President's report" in their link
+- ex: current_projects[199]
+
+
+
+'''
