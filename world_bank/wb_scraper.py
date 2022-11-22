529708bc026a0008e54372564c0c7ac133c0a1d2
3LI-N <enord@DESKTOP-5KKVVIE.localdomain>
2022-11-22 00:15:12 -0500

    getting document page urls works

--- world_bank/wb_scraper.py
+++ world_bank/wb_scraper.py
@@ -3,21 +3,47 @@ from bs4 import BeautifulSoup
 import re
 from urllib.request import unquote
 import os
+import csv
 
 
+def get_doc_urls(oldest_year):
+	doc_urls = []
+	with open('wb_projects.csv', mode ='r')as file:
+		csvFile = csv.reader(file)
+		for lines in csvFile:
+			if len(lines[7]) > 0:
+				try:
+					year = int(lines[7][:4])
+					if year >= oldest_year:
+						doc_url = lines[6].replace('project-detail', 'document-detail') + '?type=projects'
+						doc_urls.append(doc_url)
+				except:
+					pass
+				
 
-class Project:
-	def __init__(self, title, country, year, id, url):
-		self.title = title
-		self.country = country
-		self.year = year
-		self.id = id
-		self.url = url
-		self.doc_link = ''
+	return doc_urls
+
+def get_txt_url(url):
+	response = requests.get(url)
+	#http_match = re.findall('href=\"http://documents\.worldbank\.org/.+\"', response.text)
+	http_match = re.findall('table', response.text)
+	if len(http_match) > 0:
+		print(http_match[0])
+	'''content = BeautifulSoup(response.text, 'lxml')
+
+	td_tags = content.find_all('table')
+	for tag in td_tags:
+		print(tag)'''
+
+	print(response.text)
 
 
 def main():
-    print('main idk')
+	doc_urls = get_doc_urls(2015)
+	#print(doc_urls)
+	test_index = 5
+	print(doc_urls[test_index] + '\n')
+	get_txt_url(doc_urls[test_index])
 
 
 if __name__=="__main__":
