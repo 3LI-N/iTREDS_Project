acb745428385a96a7948d510e3edee92ecbec278
3LI-N <enord@DESKTOP-5KKVVIE.localdomain>
2023-02-20 13:09:38 -0500

    created seperate csv file for aws

--- ifad/ifad_metadata_scraper.py
+++ ifad/ifad_metadata_scraper.py
@@ -130,9 +130,10 @@ class Project:
 				f.write(self.summary)
 
 
-	def get_csv_row(self):
-		return [self.id, self.status, self.country, self.region, self.year, self.borrower, self.project_amount, self.commitment_amount, self.env_category, self.report, "\"" + str(self.summary) + "\""]
-
+	def get_csv_row(self, with_summary):
+		if with_summary:
+			return [self.id, self.status, self.country, self.region, self.year, self.borrower, self.project_amount, self.commitment_amount, self.env_category, self.report, str(self.summary)]
+		return [self.id, self.status, self.country, self.region, self.year, self.borrower, self.project_amount, self.commitment_amount, self.env_category, self.report, str(self.has_summary)]
 
 	
 
@@ -187,10 +188,17 @@ def main():
 
 	with open('ifad_metadata.csv', 'w') as csvfile:
 		filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		filewriter.writerow(['Project ID', 'Status', 'Country', 'Region', 'Year approved', 'Borrowing entity', 'Project amount (total)', 'Committment amount', 'Environmental category', 'Report', 'Has summary'])
+		for proj in working_projects:
+			#proj_row = proj.get_csv_row()
+			filewriter.writerow(proj.get_csv_row(False))
+	
+	with open('aws_ifad_metadata.csv', 'w') as csvfile:
+		filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 		filewriter.writerow(['Project ID', 'Status', 'Country', 'Region', 'Year approved', 'Borrowing entity', 'Project amount (total)', 'Committment amount', 'Environmental category', 'Report', 'Summary'])
 		for proj in working_projects:
 			#proj_row = proj.get_csv_row()
-			filewriter.writerow(proj.get_csv_row())
+			filewriter.writerow(proj.get_csv_row(True))
 
 
 if __name__=="__main__":
